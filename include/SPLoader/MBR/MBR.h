c34f668c241ba29cf8b189a88711a700925344c8
bringey <2031784+bringey@users.noreply.github.com>
2017-06-11 23:27:00 -0400

    Moved headers, added MBR header

---
+++ include/SPLoader/MBR/MBR.h
@@ -0,0 +1,24 @@
+#ifndef _SPLOADER_MBR_MBR_H
+#define _SPLOADER_MBR_MBR_H
+
+//
+// MBR Structure
+//
+
+#define MBR_BOOTSTRAP          0x0
+
+#define MBR_PARTITION1       0x1BE
+#define MBR_PARTITION2       0x1CE
+#define MBR_PARTITION3       0x1DE
+#define MBR_PARTITION4       0x1EE
+
+#define MBR_BOOTSIG_LOCATION 0x1FE
+
+//
+// MBR Boot Signature
+//
+
+#define MBR_BOOTSIG 0xAA55
+
+
+#endif
