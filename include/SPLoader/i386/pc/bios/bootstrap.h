772a3bb0e9225b271f32035fcca486c2509cc370
bringey <2031784+bringey@users.noreply.github.com>
2017-06-20 20:21:45 -0400

    die function uses string array (errortab) for printing the error type moved more stuff to bootstrap header

--- include/SPLoader/i386/pc/bios/bootstrap.h
+++ include/SPLoader/i386/pc/bios/bootstrap.h
@@ -1,13 +1,9 @@
 /*
-** SCCS ID:	@(#)bootstrap.h	1.4	10/18/09
+** File: include/SPLoader/i386/pc/bios/bootstrap.h
 **
-** File:	bootstrap.h
+** Author: Brennan Ringey
 **
-** Author:	K. Reek
-**
-** Contributor:	Warren R. Carithers, Garrett C. Smith
-**
-** Description:	Addresses where various stuff goes in memory.
+** Description:	Error codes and locations for the MBR bootstrap
 */
 
 #ifndef	_SPLOADER_I386_PC_BIOS_BOOTSTRAP_H
@@ -15,8 +11,36 @@
 
 #define BOOT_SEGMENT 0x07C0        // default BIOS addr to load boot sector
 #define BOOT_ADDRESS 0x7C00
+#define BOOT_STACK   0x4000        // stack top: 07C0:4000 (0xBC00)
 
 #define LOADER_LBA_LOCATION 0x1B8 // location of the loader LBA
 #define LOADER_SIZE_LOCATION 0x1BC // location of the loader size
+
+#define mkcode(type, char) (type << 8) | char
+
+// error types
+
+#define ET_BOOTSTRAP_BOOT  0
+#define ET_BOOTSTRAP_DEBUG 1
+
+
+// error codes
+//   byte 1: 0
+//   byte 0: error code character
+
+E_BOOTSTRAP_DEFAULT  = mkcode(ET_BOOTSTRAP_BOOT, '0')
+E_BOOTSTRAP_DISK     = mkcode(ET_BOOTSTRAP_BOOT, '1')
+E_BOOTSTRAP_NOLBA    = mkcode(ET_BOOTSTRAP_BOOT, '2')
+E_BOOTSTRAP_TOOBIG   = mkcode(ET_BOOTSTRAP_BOOT, '3')
+E_BOOTSTRAP_TOOSMALL = mkcode(ET_BOOTSTRAP_BOOT, '4')
+
+// debug codes
+//   byte 1: 1
+//   byte 0: debug code character
+
+E_BOOTSTRAP_DEBUG1 = mkcode(ET_BOOTSTRAP_DEBUG, 'A')
+E_BOOTSTRAP_DEBUG2 = mkcode(ET_BOOTSTRAP_DEBUG, 'B')
+E_BOOTSTRAP_DEBUG3 = mkcode(ET_BOOTSTRAP_DEBUG, 'C')
+
 
 #endif
