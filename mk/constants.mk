087c14bba1326b47d8dffc8ed42b5499bedcec67
bringey <2031784+bringey@users.noreply.github.com>
2017-06-26 01:22:57 -0400

    Moved platform and isa specific makefiles to the mk folder

--- mk/constants.mk
+++ mk/constants.mk
@@ -12,8 +12,8 @@ ISADIR      = $(SUBDIR)/$(ISA)
 PLATFORMDIR = $(SUBDIR)/$(ISA)-$(PLATFORM)
 
 
-ISA_MAKEFILE      = $(ISADIR)/arch.mk
-PLATFORM_MAKEFILE = $(PLATFORMDIR)/platform.mk
+ISA_MAKEFILE      = mk/$(ISA)/$(SUBDIR).arch.mk
+PLATFORM_MAKEFILE = mk/$(ISA)-$(PLATFORM)/$(SUBDIR).platform.mk
 
 MKIMAGE_DIR := tools/mkimage
 MKIMAGE := $(MKIMAGE_DIR)/mkimage
