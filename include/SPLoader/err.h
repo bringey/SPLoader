1ccebca3cfb8fdfa04d5a1dbb6fa476986a27eb8
bringey <2031784+bringey@users.noreply.github.com>
2017-07-02 00:06:00 -0400

    Added header containing error code macros

---
+++ include/SPLoader/err.h
@@ -0,0 +1,22 @@
+#ifndef _SPLOADER_ERR_H
+#define _SPLOADER_ERR_H
+
+//
+// Success, no error
+//
+#define E_SUCCESS 0
+
+//
+// A passed argument was null
+//
+#define E_ARGNULL 1
+
+//
+// A passed argument is out of bounds
+//
+#define E_ARGBOUNDS 2
+
+#ifndef __ASM__
+#endif
+
+#endif
