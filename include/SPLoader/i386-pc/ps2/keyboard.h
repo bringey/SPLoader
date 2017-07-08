dd19bd12db8aeb378ea5624a051e8ff6b1a1bf66
bringey <2031784+bringey@users.noreply.github.com>
2017-07-08 02:01:31 -0400

    Added PS/2 headers

---
+++ include/SPLoader/i386-pc/ps2/keyboard.h
@@ -0,0 +1,58 @@
+
+#ifndef _SPLOADER_I386_PC_PS2_KEYBOARD_H
+#define _SPLOADER_I386_PC_PS2_KEYBOARD_H
+
+//
+// Response Bytes (RP)
+// Bytes sent from the keyboard/device that have special meaning
+//
+#define PS2_RP_ERROR1           0x00  // key-detection error, buffer overrun
+#define PS2_RP_SELFTEST_PASSED  0xAA  // self-test passed after reset/power up
+#define PS2_RP_ECHO             0xEE  // echo
+#define PS2_RP_ACK              0xFA  // command acknowledgement
+#define PS2_RP_SELFTEST_FAILED1 0xFC  // self-test failed after reset/power up
+#define PS2_RP_SELFTEST_FAILED2 0xFF  // same as SELFTEST_FAILED1
+#define PS2_RP_RESEND           0xFE  // resend (repeat last command sent)
+#define PS2_RP_ERROR2           0xFF  // same as ERROR1
+
+#define PS2_RP_ERROR PS2_RP_ERROR1
+#define PS2_RP_SELFTEST_FAILED PS2_SELF_TEST_FAILED1
+
+//
+// PS/2 Keyboard Commands
+//
+#define PS2_CMD_SET_LED                        0xED
+#define PS2_CMD_ECHO                           0xEE
+#define PS2_CMD_GETSET_SCANCODE                0xF0
+#define PS2_CMD_IDENTIFY                       0xF2
+#define PS2_CMD_SET_TYPEMATIC                  0xF3
+#define PS2_CMD_ENABLE_SCANNING                0xF4
+#define PS2_CMD_DISABLE_SCANNING               0xF5
+#define PS2_CMD_DEFAULTS                       0xF6
+#define PS2_CMD_RESTRICT_TYPEMATIC             0xF7
+#define PS2_CMD_RESTRICT_MAKE_RELEASE          0xF8
+#define PS2_CMD_RESTRICT_MAKE                  0xF9
+#define PS2_CMD_RESTRICT_ALL                   0xFA
+#define PS2_CMD_RESTRICT_SPECIFIC_TYPEMATIC    0xFB
+#define PS2_CMD_RESTRICT_SPECIFIC_MAKE_RELEASE 0xFC
+#define PS2_CMD_RESTRICT_SPECIFIC_MAKE         0xFD
+#define PS2_CMD_RESEND                         0xFE
+#define PS2_CMD_RESET                          0xFF
+
+//
+// PS/2 Keyboard LED states
+//
+#define PS2_LED_SCROLL_LOCK 0x1
+#define PS2_LED_NUMBER_LOCK 0x2
+#define PS2_LED_CAPS_LOCK   0x4
+
+//
+// PS/2 Scancode sets
+//
+#define PS2_SCANCODE_QUERY 0 // get the current scancode set
+#define PS2_SCANCODE_SET_1 1 // set keyboard scancode set to set 1
+#define PS2_SCANCODE_SET_2 2 // ^ set 2
+#define PS2_SCANCODE_SET_3 3 // ^ set 3
+
+
+#endif
---
+++ include/SPLoader/i386-pc/ps2/ps2.h
@@ -0,0 +1,85 @@
+
+#ifndef _SPLOADER_I386_PC_PS2_PS2_H
+#define _SPLOADER_I386_PC_PS2_PS2_H
+
+//
+// PS/2 I/O Ports
+//
+#define PS2_PORT_DATA 0x60 // data port, read/write
+#define PS2_PORT_CMD  0x64 // command register, write only
+#define PS2_PORT_STAT 0x64 // status register, read only
+
+//
+// PS/2 Status register
+// Bits 4-5 are unknown (chipset-specific)
+//
+#define PS2_STAT_OUTPUT_BUFFER 0x1    // bit 0: set if output buffer is full
+#define PS2_STAT_INPUT_BUFFER  0x2    // bit 1: set if input buffer is full
+#define PS2_STAT_SYSTEM_FLAG   0x4    // bit 2: cleared by firmware after POST
+#define PS2_STAT_CMD           0x8    // bit 3: 
+#define PS2_STAT_TIMEOUT       0x40   // bit 6: set if time-out error occurred
+#define PS2_STAT_PARITY        0x80   // bit 7: set if a parity error occurred
+
+//
+// PS/2 Controller Configuration
+// The controller configuration is a byte read/written to using commands
+// PS2_CMD_READ_RAM/PS2_CMD_WRITE_RAM. Bits 3 and 7 are zero
+//
+#define PS2_CONFIG_PORT1_INT   0x1    // first port interrupts enabled
+#define PS2_CONFIG_PORT2_INT   0x2    // second port interrupts
+#define PS2_CONFIG_SYSTEM_FLAG 0x4    // system flag, should be 1
+#define PS2_CONFIG_PORT1_CLOCK 0x10   // first port clock
+#define PS2_CONFIG_PORT2_CLOCK 0x20   // second port clock
+#define PS2_CONFIG_PORT1_TRANS 0x40   // first port translation
+
+//
+// PS/2 Controller Output Port
+// Can be read/written to using commands PS2_CMD_READ_CTRL_OUT and
+// PS2_CMD_WRITE_CTRL_OUT
+//
+#define PS2_CTRLOUT_SYSTEM              0x1
+#define PS2_CTRLOUT_A20                 0x2
+#define PS2_CTRLOUT_PORT2_CLOCK         0x4
+#define PS2_CTRLOUT_PORT2_DATA          0x8
+#define PS2_CTRLOUT_PORT1_OUTPUT_BUFFER 0x10
+#define PS2_CTRLOUT_PORT2_OUTPUT_BUFFER 0x20
+#define PS2_CTRLOUT_PORT1_CLOCK         0x40
+#define PS2_CTRLOUT_PORT1_DATA          0x80
+
+
+//
+// PS/2 Controller Commands
+// Commands are written to the PS2_PORT_CMD port. Each command is at least one
+// byte. Some commands require an additional byte to be written.
+//
+#define PS2_CMD_READ_RAM        0x20
+#define PS2_CMD_WRITE_RAM       0x60
+
+#define PS2_CMD_TEST_CTRL       0xAA
+#define PS2_CMD_TEST_PORT_1     0xAB
+#define PS2_CMD_TEST_PORT_2     0xA9
+
+#define PS2_CMD_ENABLE_PORT_1   0xAE
+#define PS2_CMD_ENABLE_PORT_2   0xA8
+
+#define PS2_CMD_DISABLE_PORT_1  0xAD
+#define PS2_CMD_DISABLE_PORT_2  0xA7
+
+#define PS2_CMD_COPY_STAT_03    0xC1
+#define PS2_CMD_COPY_STAT_47    0xC2
+#define PS2_CMD_READ_CTRL_OUT   0xD0
+#define PS2_CMD_WRITE_CTRL_OUT  0xD1
+#define PS2_CMD_WRITE_PORT1_OUT 0xD2
+#define PS2_CMD_WRITE_PORT2_OUT 0xD3
+#define PS2_CMD_WRITE_PORT2_IN  0xD4
+
+// Non-standard, varies per chipset
+
+#define PS2_CMD_DIAGNOSTIC      0xAC
+#define PS2_CMD_READ_CTRL_IN    0xC0
+
+
+
+int ps2_init(void);
+
+#endif
