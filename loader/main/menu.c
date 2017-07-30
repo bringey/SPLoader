3097d94d4cd392c1c6f124ae3ed5e572226024b0
bringey <2031784+bringey@users.noreply.github.com>
2017-07-30 17:15:41 -0400

    test box drawing in menu_main

--- loader/main/menu.c
+++ loader/main/menu.c
@@ -17,6 +17,7 @@ static unsigned WHITE_ON_BLACK;
 static const char TITLE[] = "SPLoader";
 static const char VERSION[] = SPLOADER_VERSION;
 
+static void __menu_display(void);
 
 int menu_init(void) {
     // calculate colors
@@ -33,7 +34,7 @@ int menu_main(void) {
     unsigned height = con_height();
 
     // set the scroll menu
-    con_setWindow(1, height - 1);	
+    con_setWindow(1, height - 1);
 
     // print the title bar
     con_setColor(BLACK_ON_WHITE);
@@ -41,8 +42,35 @@ int menu_main(void) {
     int versionLen = width - sizeof(TITLE) - 1; // remaining space on line
     if (versionLen >= 0) {
         con_printf_at(sizeof(TITLE), 0, "%*s ", versionLen, VERSION);
-        //con_printf_at(sizeof(TITLE), 0, " %s", VERSION);
     }
 
+    __menu_display();
+
     return E_SUCCESS;
+}
+
+void __menu_display(void) {
+
+    con_setColor(WHITE_ON_BLACK);
+    con_printf_at(0, 2, " %-*s ", con_width() - 2, "Select an option:");
+
+    unsigned boxWidth = con_width() - 2;
+    unsigned boxHeight = 10;
+    con_putchar_at(1, 4, '\xDA');                    // upper-left corner
+    for (unsigned i = 0; i != boxWidth - 2; ++i) {
+        con_putchar_at(i + 2, 4, '\xC4');            // top border
+    }
+    con_putchar_at(boxWidth, 4, '\xBF');             // upper-right corner
+    for (unsigned i = 0; i != boxHeight; ++i) {
+        con_putchar_at(boxWidth, 5 + i, '\xB3');     // right border
+    }
+    con_putchar_at(boxWidth, 5 + boxHeight, '\xD9'); // bottom-right corner
+    for (unsigned i = 0; i != boxWidth - 2; ++i) {
+        con_putchar_at(i + 2, boxHeight + 5, '\xC4'); // bottom border
+    }
+    con_putchar_at(1, boxHeight + 5, '\xC0');        // bottom-left corner
+    for (unsigned i = 0; i != boxHeight; ++i) {
+        con_putchar_at(1, i + 5, '\xB3');            // left border
+    }
+
 }
