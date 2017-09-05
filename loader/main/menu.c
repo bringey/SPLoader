f5b0b65bb9b8a11dcf0a4812816842a3792a06b0
bringey <2031784+bringey@users.noreply.github.com>
2017-09-05 11:35:07 -0400

    menu demo

--- loader/main/menu.c
+++ loader/main/menu.c
@@ -2,7 +2,10 @@
 
 #include <SPLoader/main/menu.h>
 #include <SPLoader/console/out.h>
+#include <SPLoader/kbd/kbd.h>
 #include <SPLoader/err.h>
+
+#include <stdbool.h>
 
 #ifndef SPLOADER_VERSION
 // placeholder version macro
@@ -17,7 +20,20 @@ static unsigned WHITE_ON_BLACK;
 static const char TITLE[] = "SPLoader";
 static const char VERSION[] = SPLOADER_VERSION;
 
-static void __menu_display(void);
+typedef struct MenuItem_s {
+    char name[72];
+    char *temp;
+} MenuItem;
+
+typedef struct MenuContainer_s {
+    unsigned selected;
+    unsigned itemCount;
+    MenuItem *items;
+} MenuContainer;
+
+
+static void __menu_display(MenuContainer *cont);
+
 
 int menu_init(void) {
     // calculate colors
@@ -44,33 +60,91 @@ int menu_main(void) {
         con_printf_at(sizeof(TITLE), 0, "%*s ", versionLen, VERSION);
     }
 
-    __menu_display();
+    MenuContainer container;
+    container.selected = 0;
+    container.items = (MenuItem*)0x5000;
+
+    // for testing purposes
+    container.items[0].temp = "Menu Item 1";
+    container.items[1].temp = "Menu Item 2";
+    container.items[2].temp = "Menu Item 3";
+    container.itemCount = 3;
+    
+
+    KeyEvent evt;
+    bool updateDisplay = true;
+
+    for (;;) {
+        if (updateDisplay) {
+            __menu_display(&container);
+        }
+
+        kbd_waitForEvent(&evt);
+        if ((evt.flags & KEY_FLAG_PRESSED) == KEY_FLAG_PRESSED) {
+            switch (evt.key) {
+                case KEY_UP:
+                    if (container.selected != 0) {
+                        --container.selected;
+                        updateDisplay = true;
+                    }
+                    break;
+                case KEY_DOWN:
+                    if (container.selected != container.itemCount - 1) {
+                        ++container.selected;
+                        updateDisplay = true;
+                    }
+                    break;
+                default:
+                    updateDisplay = false;
+                    break;
+            }
+        }
+    }
+
+    //__menu_display();
 
     return E_SUCCESS;
 }
 
-void __menu_display(void) {
 
-    con_setColor(WHITE_ON_BLACK);
-    con_printf_at(0, 2, " %-*s ", con_width() - 2, "Select an option:");
+void __menu_display(MenuContainer *container) {
 
-    unsigned boxWidth = con_width() - 2;
-    unsigned boxHeight = 10;
-    con_putchar_at(1, 4, '\xDA');                    // upper-left corner
-    for (unsigned i = 0; i != boxWidth - 2; ++i) {
-        con_putchar_at(i + 2, 4, '\xC4');            // top border
+    unsigned width = con_width();
+    for (unsigned i = 0; i < container->itemCount; ++i) {
+        if (i == container->selected) {
+            con_setColor(BLACK_ON_WHITE);
+            con_setCursor(4, i + 4);
+        } else {
+            con_setColor(WHITE_ON_BLACK);
+        }
+        con_printf_at(0, i + 4, "    %-*s    ", width - 8, container->items[i].temp);
     }
-    con_putchar_at(boxWidth, 4, '\xBF');             // upper-right corner
-    for (unsigned i = 0; i != boxHeight; ++i) {
-        con_putchar_at(boxWidth, 5 + i, '\xB3');     // right border
-    }
-    con_putchar_at(boxWidth, 5 + boxHeight, '\xD9'); // bottom-right corner
-    for (unsigned i = 0; i != boxWidth - 2; ++i) {
-        con_putchar_at(i + 2, boxHeight + 5, '\xC4'); // bottom border
-    }
-    con_putchar_at(1, boxHeight + 5, '\xC0');        // bottom-left corner
-    for (unsigned i = 0; i != boxHeight; ++i) {
-        con_putchar_at(1, i + 5, '\xB3');            // left border
-    }
-
+    
 }
+
+
+// void __menu_display(void) {
+
+//     con_setColor(WHITE_ON_BLACK);
+//     con_printf_at(0, 2, " %-*s ", con_width() - 2, "Select an option:");
+
+//     unsigned boxWidth = con_width() - 2;
+//     unsigned boxHeight = 10;
+//     con_putchar_at(1, 4, '\xDA');                    // upper-left corner
+//     for (unsigned i = 0; i != boxWidth - 2; ++i) {
+//         con_putchar_at(i + 2, 4, '\xC4');            // top border
+//     }
+//     con_putchar_at(boxWidth, 4, '\xBF');             // upper-right corner
+//     for (unsigned i = 0; i != boxHeight; ++i) {
+//         con_putchar_at(boxWidth, 5 + i, '\xB3');     // right border
+//     }
+//     con_putchar_at(boxWidth, 5 + boxHeight, '\xD9'); // bottom-right corner
+//     for (unsigned i = 0; i != boxWidth - 2; ++i) {
+//         con_putchar_at(i + 2, boxHeight + 5, '\xC4'); // bottom border
+//     }
+//     con_putchar_at(1, boxHeight + 5, '\xC0');        // bottom-left corner
+//     for (unsigned i = 0; i != boxHeight; ++i) {
+//         con_putchar_at(1, i + 5, '\xB3');            // left border
+//     }
+
+// }
