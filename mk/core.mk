444cd1eb9064130714e76ae6bab42efc77b25664
bringey <2031784+bringey@users.noreply.github.com>
2017-10-07 18:31:53 -0400

    added OBJCOPY macro to build system

--- mk/core.mk
+++ mk/core.mk
@@ -13,6 +13,10 @@ CORE_OBJ = kbd/parse.o \
 CORE_OBJ := $(addprefix $(BUILD_DIR)/core/,$(CORE_OBJ))
 CORE_DEP := $(CORE_OBJ:.o=.d)
 
+# core.final.obj contains debugging symbols, core.elf does not
+CORE_FINAL_OBJ := core.final.o
+CORE_FINAL_OBJ := $(addprefix $(BUILD_DIR)/core/,$(CORE_FINAL_OBJ))
+
 CORE_ELF = core.elf
 CORE_ELF := $(addprefix $(BUILD_DIR)/core/,$(CORE_ELF))
 
@@ -21,12 +25,17 @@ CORE_OBJ_LIST = $(CORE_ENTRY_OBJ) \
                 $(CORE_PLAT_OBJ) \
                 $(CORE_OBJ)
 
-.PHONY: core.elf
+.PHONY: core.final.o core.elf
+
+core.final.o: $(CORE_FINAL_OBJ)
 
 core.elf: $(CORE_ELF)
 
-$(CORE_ELF): $(CORE_OBJ_LIST) $(LOADER_FINAL_OBJ) $(MARKER)
-	$(LD_V) $(LDFLAGS) -o $@ -r $(CORE_OBJ_LIST)
+$(CORE_FINAL_OBJ): $(CORE_OBJ_LIST) $(LOADER_FINAL_OBJ) $(MARKER)
+	$(LD_V) $(LDFLAGS) -o $@ -r -R $(LOADER_FINAL_OBJ) $(CORE_OBJ_LIST)
+
+$(CORE_ELF): $(CORE_FINAL_OBJ) $(MARKER)
+	$(OBJCOPY_V) -R .comment -R .eh_frame -g $(CORE_FINAL_OBJ) $@
 
 -include $(CORE_DEP)
 
