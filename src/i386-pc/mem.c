9271ac5851d53ff93d0117981724a73abe3cdf77
bringey <2031784+bringey@users.noreply.github.com>
2018-09-07 15:27:08 -0400

    temporary fix for memory manager to not use low memory

--- src/i386-pc/mem.c
+++ src/i386-pc/mem.c
@@ -18,7 +18,9 @@ size_t _mem_availableBlocks(void) {
     // and are 32-bit addresses
     for (unsigned i = 0; i != BIOS_MAP->length; ++i) {
         if (entry->type == 1 && entry->base_hi == 0) {
-            ++count;
+            if (entry->base_lo >= 0x100000) {
+                ++count;
+            }
         }
         ++entry;
     }
@@ -42,6 +44,10 @@ size_t _mem_nextBlock(size_t cont, FreeBlock *block) {
             uint32_t limit = entry->base_lo + entry->length_lo;
 
             if (base < 0x100000) {
+                // don't use this range for now
+                ++entry;
+                continue;
+                
                 // this range is in low memory and must be adjusted so that it
                 // doesn't conflict with the loader code, video ram, ROM and
                 // others.
@@ -49,9 +55,9 @@ size_t _mem_nextBlock(size_t cont, FreeBlock *block) {
                 // for most systems, this range is 0x0-0x9FC00. So we will use
                 // the range 0x20000-0x9FC00, assuming that this range fits in
                 // the current one.
-                if (base < 0x20000 && limit > 0x20000) {
-                    base = 0x20000;
-                }
+                // if (base < 0x20000 && limit > 0x20000) {
+                //     base = 0x20000;
+                // }
             }
             block->base = (void*)base;
             block->limit = (void*)limit;
