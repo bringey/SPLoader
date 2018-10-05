0cb268f6decbb7c20872081fcb8b48e9bb88bb81
bringey <2031784+bringey@users.noreply.github.com>
2018-10-05 19:51:38 -0400

    Prettied up loader.h, added locations from other source files here Copied locations defined in bootstrap.S to bootstrap.h

--- include/i386-pc/loader.h
+++ include/i386-pc/loader.h
@@ -3,43 +3,95 @@
 **
 ** Author: bringey
 **
+** Contains memory locations (for i386 systems) for various things used by the
+** loader. Note that all locations defined here exist in low memory (<0x100000)
+**
+** Define any fixed memory locations for modules here, this way things can
+** stay organized and there will less chance of conflicts.
 */
 
 #ifndef _I386_PC_LOADER_H
 #define _I386_PC_LOADER_H
 
+#include <i386-pc/stage0a/bootstrap.h>
+
+#define flataddr(seg, off) ((seg << 4) + off)
+
+// UNUSABLE ----------------------------------------------- 0x000000 - 0x0004FF
+
+// IDT ---------------------------------------------------- 0x000500 - 0x000CFF
+
+#define LOADER_IDT_ADDRESS          0x500
+
+// GDT ---------------------------------------------------- 0x000D00 - 0x000D27
+
+// Location of the loader's GDT, as well as entry offsets
+// See src/i386-pc/stage0b/early.S for how this is setup
+
+#define LOADER_GDT_ADDRESS          0xD00
+
+#define LOADER_GDT_NULL             0x00
+#define LOADER_GDT_CODE32           0x08
+#define LOADER_GDT_DATA32           0x10
+#define LOADER_GDT_CODE16           0x18
+#define LOADER_GDT_DATA16           0x20
+
+// STACK -------------------------------------------------- 0x000D28 - 0x003FFF
+
+// Program stack used by all stages, starts at 0x4000 and grows downward
+
+#define LOADER_STACK_ADDRESS        0x4000
+
+// MMAP --------------------------------------------------- 0x004000 - 0x0043FF
+
+// Memory map, acquired in stage 0b using BIOS E820
+
+// memory map
+#define LOADER_MMAP_LENGTH          0x4000
+#define LOADER_MMAP_ENTRY_SIZE      0x4004
+#define LOADER_MMAP_TABLE           0x4008
+
+// AVAILABLE ---------------------------------------------- 0x004400 - 0x004FFF
+
+// REALMODE ----------------------------------------------- 0x005000 - 0x0051FF
+
+// location of the realmode utility program (aka __int32)
+#define LOADER_REALMODE_ADDRESS     0x5000
+
+// AVAILABLE ---------------------------------------------- 0x005200 - 0x007BFF
+
+// MBR ---------------------------------------------------- 0x007C00 - 0x007DFF
+
+#define LOADER_MBR_ADDRESS          0x7C00
+
+// BOOT_HEADER -------------------------------------------- 0x007E00 - 0x007FFF
+
+#define LOADER_BOOT_HEADER_ADDRESS  flataddr(BOOT_SEGMENT, BOOT_BOOTHEADER)
+
+// DAP, DP ------------------------------------------------ 0x008000 - 0x0081FF
+
+#define LOADER_DP_ADDRESS           flataddr(BOOT_SEGMENT, BOOT_DP)
+#define LOADER_DAP_ADDRESS          flataddr(BOOT_SEGMENT, BOOT_DAP)
+
+// AVAILABLE ---------------------------------------------- 0x008200 - 0x00FFFF
+
+// LOADER.BIN --------------------------------------------- 0x010000 - 0x01FFFF
+
 //
 // Where loader.bin gets loaded to
 //
-#define LOADER_ADDRESS 0x10000
-//#define LOADER_STACK   0x10000
-#define LOADER_SEGMENT 0x01000
+#define LOADER_ADDRESS              0x10000
+#define LOADER_SEGMENT              0x01000
 
-//
-// Location of the Loader's stack (0000:4000)
-//
-#define LOADER_STACK_ADDRESS 0x4000
-#define LOADER_STACK_SEGMENT 0x0000
+// DD ----------------------------------------------------- 0x020000 - 0x02FFFF
 
-//
-// Location of the loader's GDT
-// 0xD00 - 0xD28
-//
-#define LOADER_GDT_SEGMENT 0x0D0
-#define LOADER_GDT_ADDRESS 0xD00
+// DD: Disk driver
+#define LOADER_DD_BUFFER            0x20000
+// buffer location segment:offset = 2000:0000
+#define LOADER_DD_SEGMENT           0x02000
+#define LOADER_DD_OFFSET            0x00000
 
-#define LOADER_GDT_NULL   0x00
-#define LOADER_GDT_CODE32 0x08
-#define LOADER_GDT_DATA32 0x10
-#define LOADER_GDT_CODE16 0x18
-#define LOADER_GDT_DATA16 0x20
 
-//
-// Location of the loader's IDT
-// 0x500 - 0xD00
-//
-#define LOADER_IDT_SEGMENT 0x050
-#define LOADER_IDT_ADDRESS 0x500
 
 #ifndef __ASM__
 #endif  // __ASM__
--- include/i386-pc/stage0a/bootstrap.h
+++ include/i386-pc/stage0a/bootstrap.h
@@ -12,6 +12,13 @@
 #define BOOT_SEGMENT 0x07C0        // default BIOS addr to load boot sector
 #define BOOT_ADDRESS 0x7C00
 
+// all locations defined here use segment 07C0 (BOOT_SEGMENT)
+
+#define BOOT_BOOTHEADER 0x200     // boot header location
+
+#define BOOT_DP 0x400             // Drive parameters structure location
+#define BOOT_DAP 0x420            // Data address packet structure location
+
 #define BOOT_LBA_LOCATION 0x1B8
 
 // error codes
