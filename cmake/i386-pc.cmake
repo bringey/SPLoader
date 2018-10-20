056bbce529af19658aa9033d5fb1a9781f125455
bringey <2031784+bringey@users.noreply.github.com>
2018-10-20 00:22:25 -0400

    add usb target (for i386-pc) to main CMakeLists.txt

--- cmake/i386-pc.cmake
+++ cmake/i386-pc.cmake
@@ -1,2 +1,20 @@
 
 set(TARGET_PREFIX "i686-elf")
+
+set(USB_IMG "usb.img")
+
+macro(custom_targets)
+
+    add_custom_command(
+        OUTPUT ${USB_IMG}
+        COMMAND ${MKIMAGE} -b ${BOOTSTRAP_BIN} ${LOADER_BIN} -o ${USB_IMG}
+        COMMENT "Making bootable usb image"
+        DEPENDS ${MKIMAGE} ${BOOTSTRAP_BIN} ${LOADER_BIN}
+    )
+
+    add_custom_target(
+        "usb"
+        DEPENDS ${USB_IMG}
+    )
+
+endmacro(custom_targets)
