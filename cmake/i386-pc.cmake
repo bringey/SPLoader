12236b8b21848ac641b74ee9ffa7d6c351fa194d
bringey <2031784+bringey@users.noreply.github.com>
2018-10-31 10:31:56 -0400

    replace mkimage with mkimage2

--- cmake/i386-pc.cmake
+++ cmake/i386-pc.cmake
@@ -15,9 +15,9 @@ macro(custom_targets)
 
     add_custom_command(
         OUTPUT ${USB_IMG}
-        COMMAND ${MKIMAGE2} -b ${BOOTSTRAP_BIN} ${STAGE1} -o ${USB_IMG}
+        COMMAND ${MKIMAGE} -b ${BOOTSTRAP_BIN} ${STAGE1} -o ${USB_IMG}
         COMMENT "Making bootable usb image"
-        DEPENDS ${MKIMAGE2} ${BOOTSTRAP_BIN} ${STAGE1}
+        DEPENDS ${MKIMAGE} ${BOOTSTRAP_BIN} ${STAGE1}
     )
 
     add_custom_target(
