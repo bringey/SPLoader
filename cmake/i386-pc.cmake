d833f9bcd3b3a0f24767dfe3872e56016fbe009a
bringey <2031784+bringey@users.noreply.github.com>
2018-10-31 10:23:10 -0400

    bootstrap uses SplHeader instead of BootHeader

--- cmake/i386-pc.cmake
+++ cmake/i386-pc.cmake
@@ -1,15 +1,23 @@
 
 set(TARGET_PREFIX "i686-elf")
 
+set(STAGE1 "stage1")
 set(USB_IMG "usb.img")
 
 macro(custom_targets)
 
     add_custom_command(
+        OUTPUT ${STAGE1}
+        COMMAND ${MKBIN} ${LOADER_BIN} -o ${STAGE1} --mbr
+        COMMENT "Making stage 1 binary"
+        DEPENDS ${MKBIN} ${LOADER_BIN}
+    )
+
+    add_custom_command(
         OUTPUT ${USB_IMG}
-        COMMAND ${MKIMAGE} -b ${BOOTSTRAP_BIN} ${LOADER_BIN} -o ${USB_IMG}
+        COMMAND ${MKIMAGE2} -b ${BOOTSTRAP_BIN} ${STAGE1} -o ${USB_IMG}
         COMMENT "Making bootable usb image"
-        DEPENDS ${MKIMAGE} ${BOOTSTRAP_BIN} ${LOADER_BIN}
+        DEPENDS ${MKIMAGE2} ${BOOTSTRAP_BIN} ${STAGE1}
     )
 
     add_custom_target(
